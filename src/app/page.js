6fde3ce71020e26a855b5912835eb2b8c5487486
Anawar Hossain <anwarhossainhossain01@gmail.com>
2026-07-06 01:13:26 +0600

    refactor Home and TodosPage components to streamline Supabase client usage and add TodosData component for task management

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,5 @@
 import SignupCard from "@/components/SignupCard";
-import Image from "next/image";
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
 
 export default async function Home() {
-  
-
-  const cookieStore = await cookies();
-  const supabase = createClient(cookieStore);
-  const { data } = await supabase.from("todos").select("title");
-  console.log(data);
-  return (
-    <SignupCard/>
-  );
+  return <SignupCard />;
 }
